c982a2031568cdabea79faa6977e3d594a204fba
Graeme Coupar <grambo@grambo.me.uk>
2014-09-11 16:41:17 +0100

    Support for additional gunicorn options.
    via ADDITIONAL_GUNICORN_OPTS environment variable.

--- gunicorn/run_gunicorn.sh
+++ gunicorn/run_gunicorn.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec /sbin/setuser app /usr/local/bin/gunicorn -w $NUM_WORKERS -b 0.0.0.0:8080 --chdir $APP_DIR $APP_NAME
+exec /sbin/setuser app /usr/local/bin/gunicorn -w $NUM_WORKERS -b 0.0.0.0:8080 --chdir $APP_DIR $ADDITIONAL_GUNICORN_OPTS $APP_NAME
